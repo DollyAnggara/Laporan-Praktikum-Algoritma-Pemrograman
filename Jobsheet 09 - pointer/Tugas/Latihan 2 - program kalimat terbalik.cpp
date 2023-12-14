94839f8bfd3c5c24406e010ab8a16c7e924556f1
DollyAnggara <145577242+DollyAnggara@users.noreply.github.com>
2023-12-14 13:10:22 +0700

    Add files via upload

---
+++ Jobsheet 09 - pointer/Tugas/Latihan 2 - program kalimat terbalik.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char kalimat[100];
+
+    printf("Masukkan kalimat: ");
+    gets(kalimat);
+
+    int panjang = strlen(kalimat);
+
+    printf("Kebalikannya: ");
+    for (int i = panjang - 1; i >= 0; i--) {
+        printf("%c", kalimat[i]);
+    }
+
+    return 0;
+}
