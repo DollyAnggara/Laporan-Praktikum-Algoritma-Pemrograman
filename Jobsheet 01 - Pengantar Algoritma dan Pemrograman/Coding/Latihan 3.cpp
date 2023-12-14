3eb7dbd7de0f8b242f3e4833192448251fc23bc2
DollyAnggara <145577242+DollyAnggara@users.noreply.github.com>
2023-12-14 18:05:25 +0700

    Add files via upload

---
+++ Jobsheet 01 - Pengantar Algoritma dan Pemrograman/Coding/Latihan 3.cpp
@@ -0,0 +1,16 @@
+/*
+Nama : DOllY ANGGARA
+NIM : 23343034
+Prodi : Informatika
+*/
+
+// Kode dimulai dengan menginclude file header standar input/output.
+#include<stdio.h>
+
+// Fungsi utama, tempat eksekusi program dimulai.
+int main(){
+	// Mencetak string "hello World" ke layar.
+	printf("hello World"); 
+	// Mengembalikan nilai 0 untuk menandakan kelangsungan program.
+	return 0;
+}
